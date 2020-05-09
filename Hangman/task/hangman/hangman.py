78ec5ab1efaed47220f4aa445960cdbb5fcf6fe4
Sergey Gurman <sgurman753@gmail.com>
2020-05-09 16:11:45 -0400

    Completed stage two of hangman

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -1,7 +1,15 @@
 # Write your code here
 def greeting():
     print('H A N G M A N')
-    print('The game will be available soon.')
+
+
+def guess():
+    word = input('Guess the word: ')
+    if word == 'python':
+        print('You survived!')
+    else:
+        print('You are hanged!')
 
 
 greeting()
+guess()
