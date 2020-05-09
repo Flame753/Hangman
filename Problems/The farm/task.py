a26eacce38007cd0b5daab1626906700c93c25e0
Sergey Gurman <sgurman753@gmail.com>
2020-05-08 20:35:39 -0400

    Attempt two on Practice three for Elif statement

--- Problems/The farm/task.py
+++ Problems/The farm/task.py
@@ -9,21 +9,32 @@ chart = dict()
 tempA = 0  # Temporary number for amount of animals
 exp_animal = "None"  # Most expensive animal
 
+
+def most_expensive(dic):
+    temp_key = "Blank"
+    temp_value = 0
+    for key, val in dic.items():
+        if val > temp_value:
+            temp_value = val
+            temp_key = key
+    return temp_key, temp_value
+
+
+last_animal = most_expensive(farm_animals)  # Output (animal, cost)
+
 # Creating new Dict() that has animals name and amount of animals can be bought
 for animal, cost in farm_animals.items():
     if money >= cost:  # Have enough money
         chart[animal] = money // cost
-    else:
-        chart[animal] = 0
-        print(chart)
 
 for animal, value in chart.items():
-    if value > tempA:
+    if value < last_animal[1]:
         tempA = value
         exp_animal = animal
 
+
 if tempA == 0:
-    print(exp_animal)
+    print(exp_animal)  # Prints None
 elif tempA == 1:
     print(str(tempA) + ' ' + exp_animal)
 else:
