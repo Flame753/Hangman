9b30533585ef4f03fe208e64e58ef200670d2094
Sergey Gurman <sgurman753@gmail.com>
2020-05-09 21:13:32 -0400

    Arguments -> URL

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -1,15 +1,7 @@
 # Write your code here
 def greeting():
     print('H A N G M A N')
-
-
-def guess():
-    word = input('Guess the word: ')
-    if word == 'python':
-        print('You survived!')
-    else:
-        print('You are hanged!')
+    print('The game will be available soon.')
 
 
 greeting()
-guess()
---
+++ Problems/URL/task.py
@@ -0,0 +1,4 @@
+def create_url(host="localhost", port=443):
+    url = "https://{}:{}".format(host, port)
+    return url
+
