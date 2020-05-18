03c6e0bdd78f8ef182290ab4a0e0e1a0c69c1b4a
Sergey Gurman <sgurman753@gmail.com>
2020-05-18 15:39:47 -0400

    Added way to randomize the correct answer

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -1,11 +1,15 @@
+import random
+
+
 # Write your code here
 def greeting():
     print('H A N G M A N')
 
 
 def guess():
+    world_lis = ['python', 'java', 'kotlin', 'javascript']
     word = input("Guess the word: ")
-    if word == "python":
+    if word == random.choice(world_lis):
         print("You survived!")
     else:
         print("You are hanged!")
