659106f9a1eb4b1a1b35226698ed955650551bc3
Sergey Gurman <sgurman753@gmail.com>
2020-05-31 19:21:23 -0400

    Stage 4/8: Help is on the way

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -1,15 +1,26 @@
 import random
 
 
-# Write your code here
 def greeting():
     print('H A N G M A N')
 
 
+# Outputs random word out of a def list
+def random_word():
+    word_lis = ['python', 'java', 'kotlin', 'javascript']
+    return random.choice(word_lis)
+
+
+# Takes the first 3 char form the given string and outputs a
+# string with the first 3 char with the rest char as "-".
+def hint(word):
+    return word[:3] + "-"*(len(word)-3)
+
+
 def guess():
-    world_lis = ['python', 'java', 'kotlin', 'javascript']
-    word = input("Guess the word: ")
-    if word == random.choice(world_lis):
+    chosen_word = random_word()
+    word = input(f"Guess the word {hint(chosen_word)}: ")
+    if word == chosen_word:
         print("You survived!")
     else:
         print("You are hanged!")
@@ -17,3 +28,4 @@ def guess():
 
 greeting()
 guess()
+
