732048b1caf74668e6e21e3fbe936dae3b6fef78
Sergey Gurman <sgurman753@gmail.com>
2020-05-31 22:40:45 -0400

    5/8: Keep trying

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -11,23 +11,31 @@ def random_word():
     return random.choice(word_lis)
 
 
-# Takes the first 3 char form the given string and outputs a
-# string with the first 3 char with the rest char as "-".
-def hint(word):
-    return word[:3] + "-"*(len(word)-3)
-
-
-def guess():
-    chosen_word = random_word()
-    for rounds in range(8):
-        print(hint(chosen_word))
-        letter = input("Guess the word : ")
-    if letter == chosen_word:
-        print("You survived!")
+def letter_in_word(word, guess_letters):
+    if guess_letters in word:
+        return True
     else:
-        print("You are hanged!")
+        return False
+
+
+def main():
+    chosen_word = random_word()
+    life = 8
+    show_word = "-" * (len(chosen_word))
+
+    for rounds in range(life):
+        print(show_word)
+        letter = (input("Guess the word : "))
+        if letter_in_word(chosen_word, letter):
+            for index, value in enumerate(chosen_word):
+                if value == letter:
+                    show_word = list(show_word)
+                    show_word[index] = letter
+                    show_word = "".join(show_word)
+        else:
+            print("No such letter in the word")
 
 
 greeting()
-guess()
+main()
 
