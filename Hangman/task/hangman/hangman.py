887e635fb82035775d63b9416c706b7532aebbd5
Sergey Gurman <sgurman753@gmail.com>
2020-06-02 13:23:21 -0400

    Stage 7/8: Menu Hangman

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -6,29 +6,34 @@ hidden = "-" * (len(word))  # The hidden word on screen
 lives = 8
 letters = set()
 
-while lives > 0:
-    if hidden != word:
-        print()
-        print(hidden)
-        guess = str(input("Input a letter: "))
-        if guess not in string.ascii_lowercase:
-            if len(guess) >= 2:
-                print("You should input a single letter")
-            else:
-                print("It is not an ASCII lowercase letter")
-        elif guess in letters:
-            print("You already typed this letter")
-        elif guess not in word:
-            print("No such letter in the word")
-            lives -= 1
-        for index in range(len(word)):
-            if guess == word[index]:
-                hidden = hidden[:index] + guess + hidden[index+1:]
-    else:
-        print("You guessed the word!")
-        print("You survived!")
+while True:
+    game_state = str(input('Type "play" to play the game, "exit" to quit: '))
+    if game_state == "exit":
         break
-    letters.add(guess)
-if lives == 0 and hidden != word:
-    print("You are hanged!")
+    if game_state == "play":
+        while lives > 0:
+            if hidden != word:
+                print()
+                print(hidden)
+                guess = str(input("Input a letter: "))
+                if guess not in string.ascii_lowercase:
+                    if len(guess) >= 2:
+                        print("You should input a single letter")
+                    else:
+                        print("It is not an ASCII lowercase letter")
+                elif guess in letters:
+                    print("You already typed this letter")
+                elif guess not in word:
+                    print("No such letter in the word")
+                    lives -= 1
+                for index in range(len(word)):
+                    if guess == word[index]:
+                        hidden = hidden[:index] + guess + hidden[index+1:]
+            else:
+                print("You guessed the word!")
+                print("You survived!")
+                break
+            letters.add(guess)
+        if lives == 0 and hidden != word:
+            print("You are hanged!")
 
